43a383b73dd4dd25a619dd4b6aebcd0ced90dba7
Carliss <carl94emil@hotmail.com>
2020-03-07 11:43:54 +0100

    add async keyword

--- beatmygame/js/android.js
+++ beatmygame/js/android.js
@@ -3,16 +3,16 @@ var bpm = document.querySelector('#bpm');
 
 btnConnect.addEventListener('click', connectToPolar);
 
-function connectToPolar () {
-    device = await navigator.bluetooth.requestDevice({ filters: [{ services: ['heart_rate'] }] });
+async function connectToPolar () {
+    var device = await navigator.bluetooth.requestDevice({ filters: [{ services: ['heart_rate'] }] });
     device = await device.gatt.connect();
-    hr = await device.getPrimaryService('heart_rate');
-    ch = await hr.getCharacteristic('heart_rate_measurement');
+    var hr = await device.getPrimaryService('heart_rate');
+    var ch = await hr.getCharacteristic('heart_rate_measurement');
     ch = await ch.startNotifications();
     ch.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
 }
 
-function handleCharacteristicValueChanged(event) {
-      value = event.target.value;
-      bpm.innerHTML = 'Received ' + value.getInt16();
-    }
+function handleCharacteristicValueChanged (event) {
+    value = event.target.value;
+    bpm.innerHTML = 'Received ' + value.getInt16();
+}
