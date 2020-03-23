15725f8d61a859b8ef5279aa8d19f40d41e8a2e9
Carliss <carl94emil@hotmail.com>
2020-03-23 15:00:38 +0100

    allow all

--- beatmygame/js/android.js
+++ beatmygame/js/android.js
@@ -5,8 +5,9 @@ btnConnect.addEventListener('click', connectToPolar);
 btnConnect.addEventListener('touchstart', connectToPolar);
 
 async function connectToPolar () {
+    // filters: [{ services: ['heart_rate'] }]
     console.log('click connect')
-    var device = await navigator.bluetooth.requestDevice({ filters: [{ services: ['heart_rate'] }] });
+    var device = await navigator.bluetooth.requestDevice({ acceptAllDevices: true });
     device = await device.gatt.connect();
     var hr = await device.getPrimaryService('heart_rate');
     var ch = await hr.getCharacteristic('heart_rate_measurement');
